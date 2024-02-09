037f74fe7ecdaf90bd4d8510aa755ebec1c68436
sahinomeerr <os202422@gmail.com>
2024-02-09 20:24:06 +0300

    A to-do list app

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById('input-box');
+const addButton = document.querySelector('button');
+const list = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value != ""){
+        const listItem = document.createElement('li');
+        listItem.innerHTML = inputBox.value;
+        list.appendChild(listItem);
+        let closeIcon = document.createElement('span');
+        closeIcon.innerHTML = '\u00d7';
+        listItem.appendChild(closeIcon);
+        inputBox.value = '';
+    }
+
+    else{
+        alert('Please enter a task');
+    }
+    saveData()
+}
+
+list.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData()
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData()
+    }
+});
+
+function saveData(){
+    localStorage.setItem('data', list.innerHTML);
+}
+
+function loadData(){
+    list.innerHTML = localStorage.getItem('data');
+}
+
+loadData();
